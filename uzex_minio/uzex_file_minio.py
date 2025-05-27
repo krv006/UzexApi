17fce0614f444265abff230287ca0dbbaa6095c7
krv006 <rvkamronbek@gmail.com>
2025-05-27 14:26:42 +0500

    change

--- uzex_minio/uzex_file_minio.py
+++ uzex_minio/uzex_file_minio.py
@@ -9,6 +9,7 @@ client = Minio(
 
 bucket_name = "uzex-files"  # <<< BU YERGA HAQIQIY BUCKET NOMINI QO‘Y
 object_name = "Templates/c0f74eba-afdf-449f-0e67-08dd96cf2567/2025/05/19/30dc2da13f9c478e84cf60a95660f93e"
+# object_name1 = "Records/13b5db46-3cae-4b30-9864-c28f368e0eb4/2025.05.23/51fd9e1028b746afbb9b3319504a63b5"
 download_path = "file_from_minio.xlsx"
 
 try:
